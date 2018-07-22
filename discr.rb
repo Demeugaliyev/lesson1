b4ef90c9205938239926a6c471ecde703e5a0f3f
Demeugaliyev <m.demeugaliev@gmail.com>
2018-07-23 05:32:47 +0600

    fixed

--- discr.rb
+++ discr.rb
@@ -8,14 +8,14 @@ print "Enter c coefficient: "
 c = gets.chomp.to_f
 
 d = b**2 - 4 * a * c
+sqrt = Math.sqrt(d)
+print "Discriminant is #{d}"
 
 if d > 0
-	x1 = (-b + Math.sqrt(d)) / (2 * a)
-	x2 = (-b - Math.sqrt(d)) / (2 * a)
-	puts "Discriminant is #{d} and x1 is #{x1}, x2 is #{x2}"
+	x1 = (-b + sqrt) / (2 * a)
+	x2 = (-b - sqrt) / (2 * a)
+	puts " and x1 is #{x1}, x2 is #{x2}"
 elsif d == 0
 	x = -b / (2 * a)
-	puts "Discriminant is #{d} and x is #{x}"
-else
-	puts "Discriminant is #{d}"
+	puts " and x is #{x}"
 end	
