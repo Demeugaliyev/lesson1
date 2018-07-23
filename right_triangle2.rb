6c64c892cbf752e9a7bfedebe14eb4f9945c8ead
Demeugaliyev <m.demeugaliev@gmail.com>
2018-07-23 06:05:52 +0600

    refactored

--- right_triangle2.rb
+++ right_triangle2.rb
@@ -7,24 +7,13 @@ b = gets.chomp.to_f
 print "Enter 3rd side: "
 c = gets.chomp.to_f
 
-if a**2 == b**2 + c**2
-	print "You have right triangle. "
-	if b == c
-		print "And triangle have 2 egual sides"
+arr = [a, b, c].sort
+
+if arr[2] ** 2 == arr[0] ** 2 + arr[1] ** 2
+	print "You have right triangle"
+	if arr[0] == arr[1]
+		print " with 2 equal side"
 	end
-elsif b**2 == c**2 + a**2
-	print "You have right triangle. "
-	if c == a
-		print "And triangle have 2 egual sides"
-	end
-elsif c**2 == a**2 + b**2
-	print "You have right triangle. "
-	if b == a
-		print "And triangle have 2 egual sides"
-	end
-elsif a == b && c
-	print "You have triangle with 3 egual sides"
-else
-	print "It's famous triangle"
+elsif arr[2] == arr[0]
+	print "it's triangle with 3 equal sides"		
 end
-	
